f9058b09515acc8baf7a5b33415cc827d5a2ff43
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-09-15 04:28:01 +0000

    [example] run `fluentci init -t gleam`

--- example/.fluentci/README.md
+++ example/.fluentci/README.md
@@ -1,10 +1,11 @@
 # Gleam Pipeline
 
+[![fluentci pipeline](https://img.shields.io/badge/dynamic/json?label=pkg.fluentci.io&labelColor=%23000&color=%23460cf1&url=https%3A%2F%2Fapi.fluentci.io%2Fv1%2Fpipeline%2Fgleam_pipeline&query=%24.version)](https://pkg.fluentci.io/gleam_pipeline)
 [![deno module](https://shield.deno.dev/x/gleam_pipeline)](https://deno.land/x/gleam_pipeline)
 ![deno compatibility](https://shield.deno.dev/deno/^1.34)
 [![codecov](https://img.shields.io/codecov/c/gh/fluent-ci-templates/gleam-pipeline)](https://codecov.io/gh/fluent-ci-templates/gleam-pipeline)
 
-A ready-to-use CI/CD Pipeline for your [Gleam](https://gleam.org) projects.
+A ready-to-use CI/CD Pipeline for your [Gleam](https://gleam.run) projects.
 
 ## 🚀 Usage
 
@@ -48,10 +49,8 @@ fluentci
 You can also use this pipeline programmatically:
 
 ```ts
-import { Client, connect } from "https://esm.sh/@dagger.io/dagger@0.8.1";
-import { Dagger } from "https://deno.land/x/gleam_pipeline/mod.ts";
-
-const { check, format, test, build } = Dagger;
+import Client, { connect } from "https://sdk.fluentci.io/v0.1.7/mod.ts";
+import { check, format, test, build } from "https://pkg.fluentci.io/gleam_pipeline@v0.3.0/mod.ts";
 
 function pipeline(src = ".") {
   connect(async (client: Client) => {
---
+++ example/.fluentci/example/.fluentci/src/dagger/jobs.ts
@@ -0,0 +1,175 @@
+import Client from "@dagger.io/dagger";
+import { withDevbox } from "https://deno.land/x/nix_installer_pipeline@v0.3.6/src/dagger/steps.ts";
+
+export enum Job {
+  check = "check",
+  format = "format",
+  test = "test",
+  build = "build",
+}
+
+export const check = async (client: Client, src = ".") => {
+  const context = client.host().directory(src);
+  const ctr = withDevbox(
+    client
+      .pipeline(Job.check)
+      .container()
+      .from("alpine:latest")
+      .withExec(["apk", "update"])
+      .withExec(["apk", "add", "curl", "bash"])
+      .withMountedCache("/nix", client.cacheVolume("nix"))
+      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
+  )
+    .withMountedCache(
+      "/root/.local/share/devbox/global",
+      client.cacheVolume("devbox-global")
+    )
+    .withExec(["devbox", "global", "add", "gleam", "erlang", "rebar3"])
+    .withEnvVariable("NIX_INSTALLER_NO_CHANNEL_ADD", "1")
+    .withMountedCache("/app/build", client.cacheVolume("gleam-build"))
+    .withDirectory("/app", context, {
+      exclude: [".git", ".devbox", ".fluentci", "build"],
+    })
+    .withWorkdir("/app")
+    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && gleam check']);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export const format = async (client: Client, src = ".") => {
+  const context = client.host().directory(src);
+  const ctr = withDevbox(
+    client
+      .pipeline(Job.format)
+      .container()
+      .from("alpine:latest")
+      .withExec(["apk", "update"])
+      .withExec(["apk", "add", "curl", "bash"])
+      .withMountedCache("/nix", client.cacheVolume("nix"))
+      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
+  )
+    .withMountedCache(
+      "/root/.local/share/devbox/global",
+      client.cacheVolume("devbox-global")
+    )
+    .withExec(["devbox", "global", "add", "gleam", "erlang", "rebar3"])
+    .withEnvVariable("NIX_INSTALLER_NO_CHANNEL_ADD", "1")
+    .withMountedCache("/app/build", client.cacheVolume("gleam-build"))
+    .withDirectory("/app", context, {
+      exclude: [".git", ".devbox", ".fluentci", "build"],
+    })
+    .withWorkdir("/app")
+    .withExec([
+      "sh",
+      "-c",
+      `eval "$(devbox global shellenv)" && gleam deps download`,
+    ])
+    .withExec([
+      "sh",
+      "-c",
+      'eval "$(devbox global shellenv)" && gleam format --check src test',
+    ]);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export const test = async (client: Client, src = ".") => {
+  const context = client.host().directory(src);
+  const ctr = withDevbox(
+    client
+      .pipeline(Job.test)
+      .container()
+      .from("alpine:latest")
+      .withExec(["apk", "update"])
+      .withExec(["apk", "add", "curl", "bash"])
+      .withMountedCache("/nix", client.cacheVolume("nix"))
+      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
+  )
+    .withMountedCache(
+      "/root/.local/share/devbox/global",
+      client.cacheVolume("devbox-global")
+    )
+    .withExec(["devbox", "global", "add", "gleam", "erlang", "rebar3"])
+    .withEnvVariable("NIX_INSTALLER_NO_CHANNEL_ADD", "1")
+    .withMountedCache("/app/build", client.cacheVolume("gleam-build"))
+    .withDirectory("/app", context, {
+      exclude: [".git", ".devbox", ".fluentci", "build"],
+    })
+    .withWorkdir("/app")
+    .withExec([
+      "sh",
+      "-c",
+      `eval "$(devbox global shellenv)" && gleam deps download`,
+    ])
+    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && gleam test']);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export const build = async (client: Client, src = ".") => {
+  const context = client.host().directory(src);
+  const ctr = withDevbox(
+    client
+      .pipeline(Job.build)
+      .container()
+      .from("alpine:latest")
+      .withExec(["apk", "update"])
+      .withExec(["apk", "add", "curl", "bash"])
+      .withMountedCache("/nix", client.cacheVolume("nix"))
+      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
+  )
+    .withMountedCache(
+      "/root/.local/share/devbox/global",
+      client.cacheVolume("devbox-global")
+    )
+    .withExec(["devbox", "global", "add", "gleam", "erlang", "rebar3"])
+    .withEnvVariable("NIX_INSTALLER_NO_CHANNEL_ADD", "1")
+    .withMountedCache("/app/build", client.cacheVolume("gleam-build"))
+    .withDirectory("/app", context, {
+      exclude: [".git", ".devbox", ".fluentci", "build"],
+    })
+    .withWorkdir("/app")
+    .withExec([
+      "sh",
+      "-c",
+      `eval "$(devbox global shellenv)" && gleam deps download`,
+    ])
+    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && gleam build']);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export type JobExec = (
+  client: Client,
+  src?: string
+) =>
+  | Promise<void>
+  | ((
+      client: Client,
+      src?: string,
+      options?: {
+        ignore: string[];
+      }
+    ) => Promise<void>);
+
+export const runnableJobs: Record<Job, JobExec> = {
+  [Job.test]: test,
+  [Job.build]: build,
+  [Job.check]: check,
+  [Job.format]: format,
+};
+
+export const jobDescriptions: Record<Job, string> = {
+  [Job.check]: "Run type checking",
+  [Job.format]: "Format source code",
+  [Job.test]: "Run tests",
+  [Job.build]: "Build the project",
+};
